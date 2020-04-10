7f2ada968c31f6e93b77c1c050967d6989b0294f
Thomas Jimenez C <tom.jimenez1661@gmail.com>
2020-04-09 19:51:23 -0600

    Initial commit

---
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AdminLayoutRoutes } from './admin-layout.routing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatRippleModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSelectModule } from '@angular/material/select';
+
+// Pages
+import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AdminLayoutRoutes),
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTooltipModule,
+  ],
+  declarations: [
+    UserProfileComponent,
+    DashboardComponent
+  ]
+})
+
+export class AdminLayoutModule {}
